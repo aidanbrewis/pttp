f63d94e076caadf713457f676486d1e9841e0a08
tobybetts23 <tobybetts0@gmail.com>
2023-08-05 21:51:53 +0100

    create react app

---
+++ React/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          legal toots to the hole
+        </p>
+        <a
+          className="App-link"
+          href="tootenland"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          its done
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
