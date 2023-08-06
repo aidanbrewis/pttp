1e00ca38d2a09766e6ef2bcdde6a0547e9c18665
tobybetts23 <tobybetts0@gmail.com>
2023-08-06 01:54:19 +0100

    remove app

--- reactjs/src/screens/Deeper/Deeper.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom"
-
-const Deeper = () => {
-    return (
-        <Link to={"/"}>Home</Link>
-    )
-}
-
-export default Deeper
--- reactjs/src/screens/Home/Home.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom"
-
-const Home = () => {
-    return (
-        <Link to="/nextLevel">Deeper</Link>
-    )
-}
-
-export default Home
