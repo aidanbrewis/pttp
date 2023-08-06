f5d76b266d758f8dedd529ed527118ce0e6b4463
tobybetts23 <tobybetts0@gmail.com>
2023-08-06 02:28:37 +0100

    router implemented

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./screens/Home/Home";
+import Screen2 from "./screens/Screen2/Screen2.jsx";
+  
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Screen2" element={<Screen2 />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
-
-export default App;
+  
+export default App
---
+++ reactjs/src/screens/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import Screen2 from "../Screen2/Screen2.jsx"
+
+const Home = () => {
+
+    return (
+        <div>
+            <Link to='/Screen2' >go deeper</Link>
+        </div>
+    )
+}
+
+export default Home
