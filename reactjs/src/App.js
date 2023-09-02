47ee573b8f0fae451207d0cc9a7d24aca0e8c5fd
Aidan Brewis <aidanbrewis@gmail.com>
2023-09-02 12:05:22 +0100

    start implementing cognito auth

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -8,19 +8,48 @@ import { ThemeProvider } from '@material-ui/styles'
 import Theme from "./theme/theme";
 import Home from "./screens/Home/Home";
 import Screen2 from "./screens/Screen2/Screen2.jsx";
+import { Amplify, Auth } from 'aws-amplify';
+import { awsExports } from "./aws-exports";
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure({
+  Auth: {
+    region: awsExports.REGION,
+    userPoolId: awsExports.USER_POOL_ID,
+    userPoolWebClientId: awsExports.USER_POOL_CLIENT_ID
+  }
+})
   
 function App() {
+  const [jwtToken, setJwtToken] = useState('');
+  useEffect(() => {
+    fetchJwtToken();
+  }, []);
+  const fetchJwtToken = async () => {
+    try {
+      const session = await Auth.currentSession();
+      const token = session.getIdToken().getJwtToken();
+      setJwtToken(token);
+    } catch (error) {
+      console.log('Error fetching JWT token:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <ThemeProvider theme={Theme}>
-        <Router>
-          <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/Screen2" element={<Screen2 />} />
-          </Routes>
-        </Router>
-      </ThemeProvider>
-    </div>
+    <Authenticator>
+      {( {signOut, user} ) => (<div className="App">
+        <ThemeProvider theme={Theme}>
+          <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Screen2" element={<Screen2 />} />
+            </Routes>
+          </Router>
+        </ThemeProvider>
+        <button onClick={signOut}>Sign out</button>
+      </div>)}
+    </Authenticator>
   );
 }
   
